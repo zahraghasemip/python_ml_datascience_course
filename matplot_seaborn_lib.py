7343cabb1c6ebd05f2bd5de0448acb8945448d89
Zahra Ghasemi <zahraghasemi2487@gmail.com>
2024-09-01 09:08:52 +0330

    creating bar chart with matplot

--- matplot_seaborn_lib.py
+++ matplot_seaborn_lib.py
@@ -1,10 +1,16 @@
-import matplotlib.pyplot as pit 
+import matplotlib.pyplot as plt 
 import seaborn as sns  
-
 from pandas import DataFrame
 data = {'names':['shana' , 'mina' , 'niki' , 'fei' , 'zara' , 'lili' , 'jack'],
         'age':[20,22,28,21,24,24,25],
         'gender':['female','female','female','female','female','female','male'],
         'rank':[2,3,1,7,4,5,6]}
 df=DataFrame(data)
-print(data)
+print(df)
+
+# creating simple bar chart
+plt.bar(df['names'] , df['age'])
+plt.xlabel('Names')
+plt.ylabel('Age')
+plt.title('comparing ages')
+plt.show()
