8bc1b75135ab2caf39e1f44d5df1aa020a5c1fe1
Zahra Ghasemi <zahraghasemi2487@gmail.com>
2024-09-02 09:28:55 +0330

    creating labels

---
+++ creating_labels.py
@@ -0,0 +1,29 @@
+import numpy as np 
+import pandas as pd 
+from pandas import Series , DataFrame 
+import matplotlib.pyplot as plt 
+from pylab import rcParams 
+import seaborn as sb 
+
+
+address='.\\data\\mtcars.csv'
+cars=pd.read_csv(address)
+cars.columns = ['car_names', 'mpg','cyl','disp','hp','drat','wt','qsec','vs','am','gear','carb']
+
+mpg=cars.mpg
+
+fig=plt.figure()
+ax = fig.add_axes([.1,.1,1,1])
+
+mpg.plot()
+
+ax.set_xticks(range(32))
+ax.set_xticklabels(cars.car_names, rotation=60, fontsize='medium')
+ax.set_title('miles per gallon of cars in mtcars')
+
+ax.set_xlabel('car names')
+ax.set_ylabel('miles/gal')
+
+ax.legend(loc='best')
+
+plt.show()
